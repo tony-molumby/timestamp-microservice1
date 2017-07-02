c12abe31f51fb1216b93ab0ac99925afa646e439
System <none>
2017-07-02 10:40:46 +0000

    🎱🌽 Updated with Glitch

--- timestamp/dateService.js
+++ timestamp/dateService.js
@@ -3,16 +3,15 @@ var date_obj = {
   unix: null,
   natural: null
 }
-var regex = /^[0-9]*/g;
+var reg = /^[0-9]+$/g;
 
 var time = function(date){
-  if(regex.test(date) && date > 0){
-    var date_int = parseInt(date);
-    var d = new Date(date * 1000);
-    date_obj.unix = date_int;
+  if(reg.test(date)){
+    var d = new Date(date * 1000).toDateString();
+    date_obj.unix = parseInt(date);
     date_obj.natural = moment(d).format('MMMM DD, YYYY');
     date_obj.type = 'number';
-  } else if(typeof date == 'string' && moment(date).isValid()){
+  } else if(moment(date).isValid()){
     date_obj.unix = parseInt(moment(date).format('X'));
     date_obj.natural = moment(date).format('MMMM DD, YYYY');
     date_obj.type = 'string';
